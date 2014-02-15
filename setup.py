dd608fe740fc6283247f9ed17e91b4db8f853446
srossross <srossross@gmail.com>
2014-02-15 09:42:08 -0800

    ENH: adding python module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Test-Binstar-CI',
+    version="dev",
+    author='Sean Ross-Ross',
+    author_email='srossross@gmail.com',
+    url='http://github.com/srossross/testci',
+    packages=find_packages()
+)
+
