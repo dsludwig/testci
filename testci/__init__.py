28ebef711eb91190b0f5846f2879b165e72db2d9
srossross <srossross@gmail.com>
2014-02-15 10:49:32 -0800

    test

---
+++ testci/__init__.py
@@ -0,0 +1,11 @@
+
+
+def foo(a=False):
+    '''
+    This is a function
+    '''
+
+    if a:
+        return 2
+
+    return 1
--- testci/tests/test_foo.py
+++ testci/tests/test_foo.py
@@ -1,9 +1,9 @@
 #test_foo.py
 
 import unittest
-
+from testci import foo
 class Test(unittest.TestCase):
 
     def test_foo(self):
-        pass
         
+        foo()
